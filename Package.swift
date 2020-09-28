d773a7448215ae30ab2267e3d9304ccae91423ad
Yuropa <Yuropa@users.noreply.github.com>
2020-09-28 12:03:13 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "KituraRateLimit",
     dependencies: [
-       .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
-       .Package(url: "https://github.com/IBM-Swift/Kitura-Cache", majorVersion: 1, minor: 0),
+       .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 2, minor: 9),
+       .Package(url: "https://github.com/IBM-Swift/Kitura-Cache", majorVersion: 2, minor: 0),
    ],
    exclude: ["Example"])
